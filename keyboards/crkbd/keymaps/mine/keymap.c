68f25213cb0b1b320ab7cc17ffadcee23b7c9579
Lukas Reineke <lukas.reineke@protonmail.com>
2020-09-18 17:22:53 +0900

    meishi

--- keyboards/crkbd/keymaps/mine/keymap.c
+++ keyboards/crkbd/keymaps/mine/keymap.c
@@ -61,87 +61,74 @@ enum custom_keycodes {
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_QWERTY] = LAYOUT(
-            MO(_ADJUST),  KC_Q,  KC_W,  KC_E,  KC_R,  KC_T,
-            KC_Y,  KC_U,  KC_I,  KC_O,  KC_P,  KC_BSPC,
+        MO(_ADJUST),  KC_Q,  KC_W,  KC_E,  KC_R,  KC_T,
+        KC_Y,  KC_U,  KC_I,  KC_O,  KC_P,  KC_BSPC,
 
-            KC_LCTL,  KC_A,  KC_S,  KC_D,  KC_F,  KC_G,
-            KC_H,  KC_J,  KC_K,  KC_L, KC_SCLN,KC_QUOT,
+        KC_LCTL,  KC_A,  KC_S,  KC_D,  KC_F,  KC_G,
+        KC_H,  KC_J,  KC_K,  KC_L, KC_SCLN,KC_QUOT,
 
-            MT(MOD_LSFT, KC_GRV),  KC_Z,  KC_X,  KC_C,  KC_V,  KC_B,
-            KC_N,  KC_M,KC_COMM, KC_DOT, KC_SLSH, MT(MOD_RSFT, KC_BSLS),
+        MT(MOD_LSFT, KC_GRV),  KC_Z,  KC_X,  KC_C,  KC_V,  KC_B,
+        KC_N,  KC_M,KC_COMM, KC_DOT, KC_SLSH, MT(MOD_RSFT, KC_BSLS),
 
-            LT(_RAISE, KC_BSPC), LT(_LOWER, KC_SPC), LGUI_T(KC_ESC),
-            RCTL_T(KC_ESC), LT(_LOWER, KC_ENT), LT(_TMUX, KC_TAB)
-
-            /* KC_LGESC, LOWER, KC_SPC, */
-            /* RCTL_T(KC_ENT), RAISE, KC_RACL */
-            ),
+        LT(_RAISE, KC_BSPC), LT(_LOWER, KC_SPC), LGUI_T(KC_ESC),
+        RCTL_T(KC_ESC), LT(_LOWER, KC_ENT), LT(_TMUX, KC_TAB)
+    ),
 
     [_LOWER] = LAYOUT(
-            KC_TRNS, KC_1, KC_2, KC_3, KC_4, KC_5,
-            KC_6, KC_7, KC_8, KC_9, KC_0, KC_TRNS,
+        KC_TRNS, KC_1, KC_2, KC_3, KC_4, KC_5,
+        KC_6, KC_7, KC_8, KC_9, KC_0, KC_TRNS,
 
-            KC_TRNS, KC_EXLM, KC_AT, KC_HASH, KC_DLR, KC_PERC,
-            KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_TRNS,
+        KC_TRNS, KC_EXLM, KC_AT, KC_HASH, KC_DLR, KC_PERC,
+        KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_TRNS,
 
-            KC_TRNS, KC_NO, KC_UNDS, KC_MINS, KC_EQL, KC_PLUS,
-            KC_LBRC, KC_LCBR, KC_RCBR, KC_RBRC, KC_TRNS, KC_TRNS,
+        KC_TRNS, KC_NO, KC_UNDS, KC_MINS, KC_EQL, KC_PLUS,
+        KC_LBRC, KC_LCBR, KC_RCBR, KC_RBRC, KC_TRNS, KC_TRNS,
 
-            KC_TRNS, KC_TRNS, KC_TRNS,
-            KC_TRNS, KC_TRNS, KC_TRNS
-            ),
+        KC_TRNS, KC_TRNS, KC_TRNS,
+        KC_TRNS, KC_TRNS, KC_TRNS
+    ),
 
     [_RAISE] = LAYOUT(
-            KC_TRNS, LGUI(KC_1), LGUI(KC_2), LGUI(KC_3), LGUI(KC_4), LGUI(KC_5),
-            LGUI(KC_6), LGUI(KC_7), LGUI(KC_8), LGUI(KC_9), LGUI(KC_0), KC_TRNS,
+        KC_TRNS, LGUI(KC_1), LGUI(KC_2), LGUI(KC_3), LGUI(KC_4), LGUI(KC_5),
+        LGUI(KC_6), LGUI(KC_7), LGUI(KC_8), LGUI(KC_9), LGUI(KC_0), KC_TRNS,
 
-            KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-            KC_LEFT, KC_DOWN, KC_UP, KC_RGHT, KC_TRNS, KC_TRNS,
+        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+        KC_LEFT, KC_DOWN, KC_UP, KC_RGHT, KC_TRNS, KC_TRNS,
 
-            KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-            KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
 
-            KC_TRNS, KC_TRNS, KC_TRNS,
-            KC_TRNS, KC_TRNS, KC_TRNS
-            ),
+        KC_TRNS, KC_TRNS, KC_TRNS,
+        KC_TRNS, KC_TRNS, KC_TRNS
+    ),
 
     [_TMUX] = LAYOUT(
-            KC_NO, TMUX_1, TMUX_2, TMUX_3, TMUX_4, TMUX_5,
-            TMUX_6, TMUX_7, TMUX_8, TMUX_9, KC_NO, KC_NO,
+        KC_NO, TMUX_1, TMUX_2, TMUX_3, TMUX_4, TMUX_5,
+        TMUX_6, TMUX_7, TMUX_8, TMUX_9, KC_NO, KC_NO,
 
-            KC_NO, KC_NO, TMUX_S, KC_NO, TMUX_F, KC_NO,
-            TMUX_H, TMUX_J, TMUX_K, TMUX_L, TMUX_COLON, KC_NO,
+        KC_NO, KC_NO, TMUX_S, KC_NO, TMUX_F, KC_NO,
+        TMUX_H, TMUX_J, TMUX_K, TMUX_L, TMUX_COLON, KC_NO,
 
-            KC_NO, KC_NO, TMUX_X, TMUX_C, TMUX_V, KC_NO,
-            KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
+        KC_NO, KC_NO, TMUX_X, TMUX_C, TMUX_V, KC_NO,
+        KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
 
-            KC_TRNS, TMUX_O, TMUX_ESC,
-            KC_TRNS, KC_NO, KC_TRNS
-
-            /* //,-----------------------------------------.                ,-----------------------------------------. */
-            /*     RESET,RGBRST, KC_NO, KC_NO, KC_NO, KC_NO,                  KC_NO,KC__MUTE, KC_NO, KC_NO, KC_NO, KC_NO, */
-            /* //|------+------+------+------+------+------|                |------+------+------+------+------+------| */
-            /*   RGB_TOG,RGB_HUI,RGB_SAI,RGB_VAI,RGB_SPI,KC_NO,               KC_PAUSE,KC__VOLUP, KC_NO, KC_NO, KC_NO, KC_NO, */
-            /* //|------+------+------+------+------+------|                |------+------+------+------+------+------| */
-            /*   RGB_MOD,RGB_HUD,RGB_SAD,RGB_VAD,RGB_SPD,KC_NO,               KC_SCROLLLOCK,KC__VOLDOWN, KC_NO, KC_NO, KC_NO, RGB_RMOD, */
-            /* //|------+------+------+------+------+------+------|  |------+------+------+------+------+------+------| */
-            /*                               KC_LGUI, LOWER,KC_SPC,   KC_ENT, RAISE,KC_RALT */
-            /*                             //`--------------------'  `--------------------' */
-            ),
+        KC_TRNS, TMUX_O, TMUX_ESC,
+        KC_TRNS, KC_NO, KC_TRNS
+    ),
 
     [_ADJUST] = LAYOUT(
-            KC_TRNS, KC_F1, KC_F2, KC_F3, KC_F4, KC_F5,
-            KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, KC_F11,
+        KC_TRNS, KC_F1, KC_F2, KC_F3, KC_F4, KC_F5,
+        KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, KC_F11,
 
-            KC_F12, KC_TRNS, RGB_SAD, RGB_SAI, RGB_SPD, RGB_SPI,
-            KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+        KC_F12, KC_TRNS, RGB_SAD, RGB_SAI, RGB_SPD, RGB_SPI,
+        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
 
-            RGB_TOG, RGB_MOD, RGB_HUD, RGB_HUI, RGB_VAD, RGB_VAI,
-            KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+        RGB_TOG, RGB_MOD, RGB_HUD, RGB_HUI, RGB_VAD, RGB_VAI,
+        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
 
-            KC_TRNS, KC_TRNS, RESET,
-            KC_TRNS, KC_TRNS, KC_TRNS
-            ),
+        KC_TRNS, KC_TRNS, RESET,
+        KC_TRNS, KC_TRNS, KC_TRNS
+    ),
 };
 
 int RGB_current_mode;
